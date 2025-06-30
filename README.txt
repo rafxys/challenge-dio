2b79644015a388add217987db6179f67ffb4330d
rafxys <rafaeltopster@gmail.com>
2025-06-29 23:30:33 -0300

    Challenge Finished

---
+++ README.txt
@@ -0,0 +1,36 @@
+
+Submission Instructions
+
+1️⃣ Hero Level Classifier Challenge
+What should be used
+
+Variables
+
+Operators
+
+Loops
+
+Decision structures
+
+Objective
+Create a variable to store the hero's name and experience points (XP), then use a decision structure to display one of the following messages:
+
+If XP is less than 1,000 → Iron
+
+If XP is between 1,001 and 2,000 → Bronze
+
+If XP is between 2,001 and 5,000 → Silver
+
+If XP is between 5,001 and 7,000 → Gold
+
+If XP is between 7,001 and 8,000 → Platinum
+
+If XP is between 8,001 and 9,000 → Ascendant
+
+If XP is between 9,001 and 10,000 → Immortal
+
+If XP is greater than or equal to 10,001 → Radiant
+
+Output
+At the end, display a message like this:
+"The Hero named {name} is at the {level} level"
--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("Hello World")
+
+// variáveis que armazenam o nome e a quantidade de experiência de um herói
+let nomeHeroi = "Rafael"
+let quantExp = 10001
+let nivel = 0
+
+switch (true) {
+    case (quantExp <= 1000):
+        nivel = "Ferro"
+        break
+    
+    case (quantExp <= 2000):
+        nivel = "Bronze"
+        break
+
+    case (quantExp <= 5000):
+        nivel = "Prata"
+        break
+
+    case (quantExp <= 7000):
+        nivel = "Ouro"
+        break
+
+    case (quantExp <= 8000):
+        nivel = "Platina"
+        break
+
+    case (quantExp <= 9000):
+        nivel = "Ascendente"
+        break
+    
+    case (quantExp <= 10000):
+        nivel = "Imortal"
+        break
+    
+    case (quantExp >= 10001):
+        nivel = "Radiante"
+        break
+
+    default:
+        console.log("Você não se classifica em nenhuma das classes heróicas")
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nivel de ${nivel}`)
