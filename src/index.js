ad42030e4d639dbdfcf8d0e98897d5b8d8da729a
Rafael Galvão <rafaeltopster@gmail.com>
2025-07-03 23:46:03 -0300

    Switch case modifications

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 
 // variables that store the name and the amount of experience of a hero
-let nomeHeroi = "Rafael"
+let name = "Rafael"
 let quantExp = 10001
-let nivel = 0
+let level = 0
 
 
 // I used the switch for this case:
 switch (true) {
     case (quantExp <= 1000):
-        nivel = "Ferro"
+        level = "Iron"
         break
     
-    case (quantExp <= 2000):
-        nivel = "Bronze"
+    case (quantExp >= 1001 && quantExp <= 2000):
+        level = "Bronze"
         break
 
-    case (quantExp <= 5000):
-        nivel = "Prata"
+    case (quantExp >= 2001 && quantExp <= 5000):
+        level = "Silver"
         break
 
-    case (quantExp <= 7000):
-        nivel = "Ouro"
+    case (quantExp >= 5001 && quantExp <= 7000):
+        level = "Gold"
         break
 
-    case (quantExp <= 8000):
-        nivel = "Platina"
+    case (quantExp >= 7001 && quantExp <= 8000):
+        level = "Platinum"
         break
 
-    case (quantExp <= 9000):
-        nivel = "Ascendente"
+    case (quantExp >= 8001 && quantExp <= 9000):
+        level = "Ascendant"
         break
     
-    case (quantExp <= 10000):
-        nivel = "Imortal"
+    case (quantExp >= 9001 && quantExp <= 10000):
+        level = "Immortal"
         break
     
     case (quantExp >= 10001):
-        nivel = "Radiante"
+        level = "Radiant"
         break
 
     default:
-        console.log("Você não se classifica em nenhuma das classes heróicas")
+        console.log("You do not qualify")
 }
 
-console.log(`O herói de nome ${nomeHeroi} está no nivel de ${nivel}`)
+console.log(`The Hero named ${name} is at the ${level} level`)
